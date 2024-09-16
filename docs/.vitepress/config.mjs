be3572786931ad2789b4fc99fb549df36cbe275c
lifehackerhansol <lifehackerhansol@ds-homebrew.com>
2024-09-16 01:36:12 -0700

    Initialize VitePress
    - Add theme submodule
    - Add initial configuration files
    - Override theme-default VPHero/VPFooter with custom theme version

---
+++ docs/.vitepress/config.mjs
@@ -0,0 +1,30 @@
+/*
+  Copyright (C) 2024 Nintendo Homebrew
+  SPDX-License-Identifier: MIT
+*/
+
+import { fileURLToPath, URL } from 'node:url'
+import { defineConfig } from 'vitepress'
+
+export default defineConfig({
+  title: "Hacks Guide",
+  description: "Hacks Guide Listings",
+  vite: {
+    resolve: {
+      alias: [
+        {
+          find: /^.*\/VPHero\.vue$/,
+          replacement: fileURLToPath(
+            new URL('./theme/components/VPHero.vue', import.meta.url)
+          )
+        },
+        {
+          find: /^.*\/VPFooter\.vue$/,
+          replacement: fileURLToPath(
+            new URL('./theme/components/VPFooter.vue', import.meta.url)
+          )
+        }
+      ]
+    }
+  }
+})
